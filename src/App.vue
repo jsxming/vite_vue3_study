ded957686ae0e4ec5021afc7572c4e96c9635681
小明同学 <923727421@qq.com>
2022-04-26 09:56:01 +0800

    feat: 添加env文件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Description: 
+ * @Autor: 小明～
+ * @Date: 2022-04-26 09:08:16
+ * @LastEditors: 小明～
+ * @LastEditTime: 2022-04-26 09:45:19
+-->
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+console.warn(import.meta.env)
 </script>
 
 <template>
